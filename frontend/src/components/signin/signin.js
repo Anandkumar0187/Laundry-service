585e36db6765d92baf4658cf7db4fd99ad49816d
tsawant635 <101405324+tsawant635@users.noreply.github.com>
2022-09-24 23:14:37 +0530

    Update signin.js

--- frontend/src/components/signin/signin.js
+++ frontend/src/components/signin/signin.js
@@ -14,7 +14,7 @@ function Signin() {
   // const [data, setData] = useState({ email: "", password: "" });
   const [data, setData] = useState({ emailphone: "", password: "" });
   const [error, setError] = useState("");
-  //   const navigate = useNavigate();
+//   const navigate = useNavigate();
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
   };
@@ -23,20 +23,20 @@ function Signin() {
     e.preventDefault();
     try {
       const url = "http://localhost:8080/api/auth";
-
+     
       const { data: res } = await axios.post(url, data);
       localStorage.setItem("token", res.data[1]);
       localStorage.setItem("name", res.data[0]);
       localStorage.setItem("address", res.data[2]);
 
-      localStorage.setItem("isLoggedIn", true)
+      localStorage.setItem("isLoggedIn",true)
       console.log("isLoggedIn")
       // console.log("hi", res.data);
       console.log(res.data[1])
       window.location = "/";
     } catch (error) {
       if (
-        error.response
+        error.response 
       ) {
         setError(error.response.data.message);
       }
@@ -46,36 +46,36 @@ function Signin() {
   return (
     <>
       <Header />
-      <div className="main">
-        <div className="section-1">
-          <div className="laundrytext">
+      <div className="mainsp3">
+        <div className="section-1p3">
+          <div className="laundrytextp3">
             <h1>
               Laundry <br />
               Service
             </h1>
           </div>
-          <div className="doortext">
+          <div className="doortextp3">
             <h5>Doorstep Wash & DryClean Service</h5>
           </div>
-          <div className="donotaccounttext">
+          <div className="donotaccounttextp3">
             <h6>Don't Have An Account</h6>
           </div>
           <div>
-            <Link to="/register">
-              <button type="button" className="registerbtn">Register</button>
+          <Link to="/register">
+            <button type="button" className="registerbtnp3">Register</button>
             </Link>
           </div>
         </div>
-        <div className="section-2">
-          <div className="sign-heading-text-div">
-            <h4 className="sign-heading-text">SIGN IN</h4>
+        <div className="section-2p3">
+          <div className="sign-heading-text-divp3">
+            <h4 className="sign-heading-textp3">SIGN IN</h4>
           </div>
 
-          <div className="form-div">
-            <div className="form-div-2">
+          <div className="form-divp3">
+            <div className="form-div-2p3">
               <form onSubmit={handleSubmit}>
                 <input
-                  className="box-input-field-2"
+                  className="box-input-field-2p3"
                   placeholder="Mobile / Email"
                   type="text"
                   value={data.emailphone}
@@ -83,9 +83,9 @@ function Signin() {
                   onChange={handleChange}
                   required
                 />
-                {error && <div className={"errmsg"}>{error}</div>}
+               {error && <div className={"errmsg"}>{error}</div>}
                 <input
-                  className="box-input-field-2"
+                  className="box-input-field-2p3"
                   type="password"
                   placeholder="Password"
                   name="password"
@@ -93,24 +93,24 @@ function Signin() {
                   value={data.password}
                   required
                 />
-                <img src="./images/padlock.svg" className="padlock" alt="" />
-                <div className="forget-password-text">Forget Password?</div>
-                <button type="submit" className="signin-btn">
-                  Sign In
-                </button>
-              </form>
+               <img src="./images/padlock.svg"  className="padlockp3" alt="" />
+              <div className="forget-password-textp3">Forget Password?</div>
+              <button type="submit" className="signin-btnp3">
+              Sign In
+            </button>
+            </form>
             </div>
           </div>
-
-
-
-
-
+          
+           
+            
+            
+          
         </div>
-
+        
       </div>
-      <Referral />
-      <Footer />
+      <Referral/>
+     <Footer/>
       <Copyright />
     </>
   );
